3be73d175b9690dc94145e07c538d100c149fde4
JJ Bussert <jj.bussert@gmail.com>
2023-05-01 11:39:03 -0500

    fixed jj-run

--- src/envrun.js
+++ src/envrun.js
@@ -1,10 +1,13 @@
 // index.js
 import { envset } from 'jj-envset';
 import { execSync } from 'child_process';
+import { run } from 'jj-run';
 
 function envrun(targetKey, command, envFile) {
     // Execute the command and get its standard output
-    const commandOutput = execSync(command).toString().trim();
+    //const commandOutput = execSync(command).toString().trim();
+    
+    const commandOutput = run(command, envFile);
 
     envset(targetKey, commandOutput, envFile);
 }
