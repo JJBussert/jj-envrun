79007d29b9c2ff36d2f44da77b03695b3e0f6999
JJ Bussert <jj.bussert@gmail.com>
2023-05-05 11:43:27 -0500

    bugfix, verbose & envfile were reversed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import { envset } from 'jj-envset';
 import { run } from 'jj-run';
 
 function envrun(targetKey, command, verbose, envFile) {
-  const commandOutput = run(command, verbose, envFile);
+  if(verbose) {
+    console.log(`targetKey ${targetKey}`);
+    console.log(`command ${command}`);
+    console.log(`envFile ${envFile}`);
+  }
+  const commandOutput = run(command, envFile, verbose);
 
   envset(targetKey, commandOutput, verbose, envFile);
 }
