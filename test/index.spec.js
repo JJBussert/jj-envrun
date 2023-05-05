578815c5ad440ea98d9b64313b3f73ed45608b21
JJ Bussert <jj.bussert@gmail.com>
2023-05-05 11:43:37 -0500

    corrected unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,73 +1,76 @@
 import { expect, test } from 'vitest';
 import { envrun } from '../src/index.js';
 import fs from 'fs';
-import { execSync } from 'child_process';
 
 test('envrun: should save the output of the command to the specified key in the env file', async () => {
-  const targetKey = 'HELLO_WORLD';
-  const command = 'echo Hello World!';
-  const envFile = '.env.test';
-
-  // Remove the envFile if it exists
-  if (fs.existsSync(envFile)) {
+  // arrange
+  const envFile = '.env.test1';
+  
+  if (fs.existsSync(envFile)) {// Remove the envFile if it exists
     fs.unlinkSync(envFile);
   }
 
-  // Call the envrun function
-  envrun(targetKey, command, false, envFile);
+  // act
+  envrun('NEW_KEY', 'echo New Content', false, envFile);
 
-  // Read the updated env file
+  // assert
   const envContent = fs.readFileSync(envFile, 'utf-8');
-  const expectedOutput = `${targetKey}="${execSync(command).toString().trim()}"`;
+  expect(envContent).toContain('NEW_KEY="New Content"');
 
-  // Check if the target key and value are present in the env file
-  expect(envContent).toContain(expectedOutput);
-
-  // Clean up by removing the envFile
+  // cleanup
   fs.unlinkSync(envFile);
 });
 
-test('envrun: should update an existing env file with new content', async () => {
-  const targetKey = 'NEW_KEY';
-  const command = 'echo Updated Value!';
-  const envFile = '.env.test';
+test('envrun: should update an existing env file with a new var', async () => {
+  
+  // arrange
+  const envFile = '.env.test2';
+  fs.writeFileSync(envFile, 'UNIT_TEST="Hello World"\n');
 
-  // Create an initial env file with some content
-  fs.writeFileSync(envFile, 'INITIAL_KEY="Initial Value"\n');
+  // act
+  envrun('NEW_KEY', 'echo New Content', false, envFile);
 
-  // Call the envrun function to update the env file with new content
-  envrun(targetKey, command, false, envFile);
-
-  // Read the updated env file
+  // assert
   const envContent = fs.readFileSync(envFile, 'utf-8');
-  const expectedOutput = `${targetKey}="${execSync(command).toString().trim()}"`;
+  expect(envContent).toContain('NEW_KEY="New Content"');
+  expect(envContent).toContain('UNIT_TEST="Hello World"'); // check if original key is unaffected
 
-  // Check if the target key and value are present in the env file
-  expect(envContent).toContain(expectedOutput);
-  expect(envContent).toContain('INITIAL_KEY="Initial Value"');
+  // cleanup
+  fs.unlinkSync(envFile);
+});
 
-  // Clean up by removing the envFile
+test('envrun: should update an existing env file with new content from existing environment var', async () => {
+
+  // arrange
+  const envFile = '.env.test3';
+  fs.writeFileSync(envFile, 'UNIT_TEST="Hello World"\n');
+
+  // act
+  envrun('NEW_KEY', 'echo "$UNIT_TEST"', false, envFile);
+
+  // assert
+  const envContent = fs.readFileSync(envFile, 'utf-8');
+  expect(envContent).toContain('NEW_KEY="Hello World"');
+  expect(envContent).toContain('UNIT_TEST="Hello World"'); // check if original key is unaffected
+
+  // cleanup
   fs.unlinkSync(envFile);
 });
 
 test('envrun: should update an quote existing values if they were not', async () => {
-  const targetKey = 'NEW_KEY';
-  const command = 'echo Updated Value!';
-  const envFile = '.env.test';
 
-  // Create an initial env file with some content
-  fs.writeFileSync(envFile, 'INITIAL_KEY=Initial Value\n');
+  // arrange
+  const envFile = '.env.test4';
+  fs.writeFileSync(envFile, 'UNIT_TEST=Hello World\n'); // arranged file will not be quoted
 
-  // Call the envrun function to update the env file with new content
-  envrun(targetKey, command, false, envFile);
+  // act
+  envrun('NEW_KEY', 'echo "$UNIT_TEST"', false, envFile);
 
-  // Read the updated env file
+  // assert
   const envContent = fs.readFileSync(envFile, 'utf-8');
-  const expectedOutput = `${targetKey}="${execSync(command).toString().trim()}"`;
+  expect(envContent).toContain('NEW_KEY="Hello World"');
+  expect(envContent).toContain('UNIT_TEST="Hello World"'); // check if original key is now quoted
 
-  // Check if the target key and value are present in the env file
-  expect(envContent).toContain('INITIAL_KEY="Initial Value"');
-
-  // Clean up by removing the envFile
+  // cleanup
   fs.unlinkSync(envFile);
 });
